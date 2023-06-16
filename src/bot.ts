383855082a36bba9c693d7ddd37325a2283c8a3e
Nazar Antoniuk <antonuk.nazar09@gmail.com>
2023-06-16 11:31:23 +0300

    feat: deletes conversations because they conflict with the menu

--- src/bot.ts
+++ src/bot.ts
@@ -1,24 +1,15 @@
 import "https://deno.land/std@0.190.0/dotenv/load.ts";
 import { Bot, session } from "grammy";
-import { conversations, createConversation } from "grammy/conversations";
 import type { MyContext } from "./types.ts";
 import { translate } from "./lib/translate.ts";
-import { alphabetConvesation } from "./conversations/alphabet.ts";
 import { alphabetsMenu } from "./menus/alphabets.ts";
 
 const bot = new Bot<MyContext>(Deno.env.get("BOT_TOKEN") || "");
 // @ts-expect-error: some problem with recognizing a string as an AlphabetName type
 bot.use(session({ initial: () => ({ alphabetName: "prudeus" }) }));
 bot.use(alphabetsMenu);
-bot.use(conversations());
-bot.use(createConversation(alphabetConvesation, "alphabet"));
 
 bot.command("start", (ctx) => ctx.reply("hello"));
-bot.command("alphabet", (ctx) => ctx.conversation.enter("alphabet"));
-bot.command("cancel", async (ctx) => {
-  await ctx.conversation.exit();
-  await ctx.reply("Операція скасована.");
-});
 bot.on("message:text", (ctx) =>
   ctx.reply(
     translate(ctx.message.text, ctx.session.alphabetName),
--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,6 @@
 import type { Context, SessionFlavor } from "grammy";
-import type { ConversationFlavor, ConversationFn } from "grammy/conversations";
 
-export type MyContext =
-  & Context
-  & SessionFlavor<{ alphabetName: AlphabetName }>
-  & ConversationFlavor;
-export type MyConversationFn = ConversationFn<MyContext>;
+export type MyContext = Context & SessionFlavor<{ alphabetName: AlphabetName }>;
 export type AlphabetName = "prudeus" | "passport" | "geography" | "iso9";
 export type Alphabet = Record<string, string | { start: string; other: string }>;
 export type Alphabets = Record<AlphabetName, Alphabet>;
